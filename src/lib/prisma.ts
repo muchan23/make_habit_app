9218abb82164312dc9936aaa793a7f41f36a4d21
muchan23 <murakami-yuki866@g.ecc.u-tokyo.ac.jp>
2025-09-28 12:23:00 +0900

    update api

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,11 +6,6 @@ const globalForPrisma = globalThis as unknown as {
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({
     log: ['query', 'info', 'warn', 'error'],
-    datasources: {
-        db: {
-            url: process.env.DATABASE_URL,
-        },
-    },
 });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
